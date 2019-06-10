937b751bc1053740a5cda2160412842521a5675c
ChironIsBitch <39786767+ChironIsBitch@users.noreply.github.com>
2019-06-10 18:34:49 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,31 +5,37 @@ import auth
 import requests
 import json
 
+
+
+
 appid = '16448'
 secret_key =  'gfehWKydKGhaZKNv45fjbRNALM4'
 
 if __name__ == '__main__':
     auth_key = str(auth.gen_sign(appid, secret_key, time.time()+3600), encoding='utf-8')
-
-
-    data = {
-        'base': {
-            'appid': appid,
-            'auth_key': auth_key,
-            'cmds': ['NlpTts']
+    while 1:
+        qq = input()
+        data = {
+            'base': {
+                'appid': appid,
+                'auth_key': auth_key,
+                'cmds': ['NlpTts'],
+                'speed' :'faster',
+                },
+            'media':qq,
+            "params": {
+                    "NlpTts": {
+                            "mode": "Daji",
+                            "speed":"normal",
+                            
+                    }
             },
-        'params': {
-            'mode': 'Daji',
-        },
-        'media': '梦想还是要有的,万一实现了呢',
 
-    }
 
-    rsp = requests.post('http://openai.qq.com/api/json/ai/GetMultiAI', json=data)
-    print(rsp.text)
-    print(type(rsp.text))
-    shuju = json.loads(rsp.text)
-    print(shuju)
-    print(shuju['cmd_rsps']['NlpTts']['data']['voice_url'])
+        }
+
+        rsp = requests.post('http://openai.qq.com/api/json/ai/GetMultiAI', json=data)
+        shuju = json.loads(rsp.text)
+        print(shuju['cmd_rsps']['NlpTts']['data']['voice_url'])
 
 
